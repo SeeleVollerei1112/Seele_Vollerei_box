2610c213925de3e85afb73b98fa9205b9461063a
Seele-Volleria <2486545465@qq.com>
2023-04-23 14:17:10 +0800

    <第一次上传>

---
+++ 1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    int g,s;
+    printf("请输入一个两位数；");
+    scanf_s("%d",&i);
+    g = i / 10;
+    s = i % 10;
+    printf("%d的十位是%d,个位是%d",i , g ,s);
+
+    return 0;
+}
+// QWQ终于提交好了，感觉自己好笨QWQ
+// 第二次提交，我感觉我又行了
